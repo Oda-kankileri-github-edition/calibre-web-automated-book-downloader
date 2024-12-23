2e87c60a13337b5259b44ce9c1be515551b7c1ab
Sıddık AÇIL <sddkacil.1@gmail.com>
2024-12-23 22:39:53 +0100

    chore(workflows): Change Action Setup (#3)
    * chore(workflows): Enable pipeline runs on every push
    
    * chore(workflows): Introduce a Dockerfile for the Rust app
    
    * chore(workflows): Change the build-push-action context as well
    
    * chore(workflows): Change the build-push-action context and file

--- calibre-web-automated-downloader/src/main.rs
+++ calibre-web-automated-downloader/src/main.rs
@@ -4,9 +4,6 @@ use config::CONFIG;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Access configuration settings using the global CONFIG instance
-    let config = CONFIG.as_ref().expect("Failed to load configuration");
-
-    println!("Base Directory: {:?}", config.base_dir);
-
+    println!("Base Directory: {:?}", CONFIG.base_dir);
     Ok(())
 }
